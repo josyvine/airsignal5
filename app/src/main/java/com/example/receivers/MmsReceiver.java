924a0eb9067eb5d68d223a2b4405ff9f97f57caf
josyvine <64621103+josyvine@users.noreply.github.com>
2026-09-03 17:55:51 +0530

    Add app/src/main/java/com/example/receivers/MmsReceiver.java via FastGit Mobile App

---
+++ app/src/main/java/com/example/receivers/MmsReceiver.java
@@ -0,0 +1,12 @@
+package com.example.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Required stub for Default SMS App status in Android OS
+    }
+}
