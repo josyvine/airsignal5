8c2b84e62ad2a9349d02f522a60286484874002c
josyvine <64621103+josyvine@users.noreply.github.com>
2026-09-03 17:55:59 +0530

    Add app/src/main/java/com/example/services/MessageSyncService.java via FastGit Mobile App

---
+++ app/src/main/java/com/example/services/MessageSyncService.java
@@ -0,0 +1,17 @@
+package com.example.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MessageSyncService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+}
